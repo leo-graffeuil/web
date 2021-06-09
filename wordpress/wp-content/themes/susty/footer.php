dfd1e85bdd3eb2ea91881457c0cd6793c3b0d1b4
Aurélien Masfrand <aurelien@toovalu.com>
2021-06-10 00:45:00 +0200

    footer ok

--- wordpress/wp-content/themes/susty/footer.php
+++ wordpress/wp-content/themes/susty/footer.php
@@ -21,12 +21,27 @@
         </div>
     </div>
 
-    <?php
-    wp_nav_menu(array(
-        'theme_location' => 'menu-4',
-        'menu_id'        => 'secondary-menu',
-    ));
-    ?>
+    <div id="subfooter">
+        <div class="col col-2">
+            <?php
+            wp_nav_menu(array(
+                'theme_location' => 'menu-2',
+                'menu_id'        => 'footer-menu',
+            ));
+            ?>
+            <p id="footer_legal">&copy;2021 TOOVALU - &copy; Crédit photo Charlotte Goislot</p>
+        </div>
+        <div class="col">
+            <ul id="footer_sharing">
+                <li><a href="#" class="icon"><span class="dashicons dashicons-facebook-alt"></span></a></li>
+                <li><a href="#" class="icon"><span class="dashicons dashicons-linkedin"></span></a></li>
+                <li><a href="#" class="icon"><span class="dashicons dashicons-twitter"></span></a></li>
+            </ul>
+        </div>
+        <div class="col">
+            <img id="footer_logo" src="<?php echo get_template_directory_uri(); ?>/images/Toovalu_footer.png" alt="logo Toovalu">
+        </div>
+    </div>
 </footer>
 </div>
 
