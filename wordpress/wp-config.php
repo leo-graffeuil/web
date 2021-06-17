4990c586d7e7385112107726306d2b6a96a4bc9d
Aurélien Masfrand <aurelien@toovalu.com>
2021-06-17 15:13:10 +0200

    rollback wp config

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -29,13 +29,13 @@ if (!function_exists('getenv_docker')) {
 	// https://github.com/docker-library/wordpress/issues/588 (WP-CLI will load this file 2x)
 	function getenv_docker($env, $default)
 	{
-		if (($val = getenv($env)) !== false) {
-			return $val;
-		}
 		if ($fileEnv = getenv($env . '_FILE')) {
 			return rtrim(file_get_contents($fileEnv), "\r\n");
+		} else if (($val = getenv($env)) !== false) {
+			return $val;
+		} else {
+			return $default;
 		}
-		return $default;
 	}
 }
 
