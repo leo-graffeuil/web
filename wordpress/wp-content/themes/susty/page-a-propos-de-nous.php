88c243cdf79636b3d8dec1bbf679c601ba5441f6
anais0210 <a.sparesotto@icloud.com>
2021-06-18 14:40:06 +0200

    modification demandé par stéphanie Jeudi 17 juin

--- wordpress/wp-content/themes/susty/page-a-propos-de-nous.php
+++ wordpress/wp-content/themes/susty/page-a-propos-de-nous.php
@@ -15,41 +15,27 @@
 
 get_header();
 ?>
-<?php get_template_part('template-parts/content', 'page'); ?>
 
 <div id="primary">
     <main id="main">
-        <div class="container">
-            <div class="bg-pink pb-4">
-                <div class="text-center">
-                    <h2 class="color-blue">
-                        <em class="">Ils témoignent</em>
-                    </h2>
-                </div>
-                <h3 class="text-center"><strong>BNP PARIBAS REAL ESTATE</strong></h3>
-                <p class="text-center">«Un outil fiable, traçable, simple, clair, efficace»</p>
-            </div>
+        <div class="content banniere">
+            <h1 class="banniere-text">A propos de nous</h1>
+            <img class="banniere-img" src="/wp-content/uploads/2021/06/universite-toovalu.jpeg" alt="">
         </div>
-        <h2 class="text-center"><em>L'entreprise</em> Toovalu</h2>
-        <p class="text-center ml-15 mr-15 pb-6">Notre nom a été inspiré par l’archipel Tuvalu situé dans l’océan pacifique. Triste <br>
-            symbole du réchauffement climatique avec la conséquence de la montée des eaux.<br>
-            Dans 50 ans, Tuvalu disparaitra si nous n’agissons pas !<br>
-            Les premiers mais pas les derniers, alors agissons !</p>
-        <h2 class="text-center pb-4">Une équipe motivée, dynamique<br> et solidaire !</h2>
         <div class="grid4">
             <div class="">
                 <h3 class="color-green"><em>Marie</em></h3>
                 <p class="text-center">Directrice générale</p>
             </div>
             <div>
-                <img src="/wp-content/uploads/2021/04/394-591-max-3.jpeg" alt="">
+                <img src="/wp-content/uploads/2021/04/70.png" alt="">
             </div>
             <div class="">
                 <h3 class="color-green"><em>François</em></h3>
                 <p class="text-center">Président de TOOVALU</p>
             </div>
             <div class="grid_fullcell">
-                <img src="/wp-content/uploads/2021/06/Capture-décran-2021-05-19-à-11.54.32.png" alt="">
+                <img src="/wp-content/uploads/2021/06/70.png" alt="">
             </div>
             <div class="grid_fullcell">
                 <img src="/wp-content/uploads/2021/06/80.png" alt="">
@@ -59,7 +45,7 @@ get_header();
                 <p class="text-center">Adjointe de direction</p>
             </div>
             <div class="grid_fullcell">
-                <img src="/wp-content/uploads/2021/06/Capture-décran-2021-06-10-à-11.11.22.png" alt="">
+                <img src="/wp-content/uploads/2021/06/70.png" alt="">
             </div>
             <div class="">
                 <h3 class="color-green"><em>Renate</em></h3>
@@ -76,83 +62,76 @@ get_header();
                 <h3 class="color-green"><em>Quentin</em></h3>
                 <p class="text-center">Président de TOOVALU</p>
             </div>
-            <div class="grid_fullcell">
-                <img src="/wp-content/uploads/2021/06/Capture-décran-2021-06-10-à-11.11.22.png" alt="">
+        </div>
+        <div class="CSSgal">
+
+            <!-- Don't wrap targets in parent -->
+            <s id="s1"></s>
+            <s id="s2"></s>
+            <s id="s3"></s>
+            <s id="s4"></s>
+
+            <div class="slider">
+                <h2 class="text-center"><em>L'entreprise</em> Toovalu</h2>
+                <p class="text-center ml-15 mr-15 pb-6">Notre nom a été inspiré par l’archipel Tuvalu situé dans l’océan pacifique. Triste <br>
+                    symbole du réchauffement climatique avec la conséquence de la montée des eaux.<br>
+                    Dans 50 ans, Tuvalu disparaitra si nous n’agissons pas !<br>
+                    Les premiers mais pas les derniers, alors agissons !</p>
+                <h2 class="text-center pb-4">Une équipe motivée, dynamique<br> et solidaire !</h2>
+                <div class="flex-space-around pb-2">
+                    <a href="#sales">Sales</a>
+                    <a href="#succes">Succes</a>
+                    <a href="coordination">Coordination</a>
+                    <a href="scrum">Scrum</a>
+                </div>
+                <div style="background:#85b;">
+                    <h2>Slide 2</h2>
+                </div>
+                <div style="background:#e95;">
+                    <h2>Slide 3</h2>
+                </div>
+                <div style="background:#e59;">
+                    <h2>Slide 4</h2>
+                </div>
             </div>
 
-            <div class="grid_fullcell">
-                <img src="/wp-content/uploads/2021/06/80.png" alt="">
-            </div>
-            <div class="">
-                <h3 class="color-green"><em>Stéphanie</em></h3>
-                <p class="text-center">Adjointe de direction</p>
-            </div>
-            <div class="grid_fullcell">
-                <img src="/wp-content/uploads/2021/06/Capture-décran-2021-06-10-à-11.11.22.png" alt="">
-            </div>
-            <div class="">
-                <h3 class="color-green"><em>Renate</em></h3>
-                <p class="text-center">Business developer RSE</p>
-            </div>
-            <div class="">
-                <h3 class="color-green"><em>Elodie</em></h3>
-                <p class="text-center">Succes manager</p>
-            </div>
-            <div class="grid_fullcell">
-                <img src="/wp-content/uploads/2021/06/Capture-décran-2021-06-10-à-11.11.22.png" alt="">
-            </div>
-            <div class="">
-                <h3 class="color-green"><em>Mélodie</em></h3>
-                <p class="text-center">Business developer Climat</p>
-            </div>
-            <div class="grid_fullcell">
-                <img src="/wp-content/uploads/2021/06/Capture-décran-2021-06-10-à-11.11.22.png" alt="">
+            <div class="prevNext">
+                <div><a href="#s4"></a><a href="#s2"></a></div>
+                <div><a href="#s1"></a><a href="#s3"></a></div>
+                <div><a href="#s2"></a><a href="#s4"></a></div>
+                <div><a href="#s3"></a><a href="#s1"></a></div>
             </div>
 
-            <div class="grid_fullcell">
-                <img src="/wp-content/uploads/2021/06/80.png" alt="">
+            <div class="bullets">
+                <a href="#s1">1</a>
+                <a href="#s2">2</a>
+                <a href="#s3">3</a>
+                <a href="#s4">4</a>
             </div>
-            <div class="">
-                <h3 class="color-green"><em>Stéphanie</em></h3>
-                <p class="text-center">Adjointe de direction</p>
-            </div>
-            <div class="grid_fullcell">
-                <img src="/wp-content/uploads/2021/06/Capture-décran-2021-06-10-à-11.11.22.png" alt="">
-            </div>
-            <div class="">
-                <h3 class="color-green"><em>Renate</em></h3>
-                <p class="text-center">Business developer RSE</p>
-            </div>
-            <div class="">
-                <h3 class="color-green"><em>Elodie</em></h3>
-                <p class="text-center">Succes manager</p>
-            </div>
-            <div class="grid_fullcell">
-                <img src="/wp-content/uploads/2021/06/Capture-décran-2021-06-10-à-11.11.22.png" alt="">
-            </div>
-            <div class="">
-                <h3 class="color-green"><em>Mélodie</em></h3>
-                <p class="text-center">Product owner</p>
-            </div>
-            <div class="grid_fullcell">
-                <img src="/wp-content/uploads/2021/06/Capture-décran-2021-06-10-à-11.11.22.png" alt="">
-            </div>
+
         </div>
         <div class="bg-green">
             <h2 class="text-center"><em>10 ans</em> d'histoire</h2>
         </div>
         <div class="grid2 pb-6">
+            <div class="">
+                <h3 class="text-center mt-15">Notre <em>organisation</em></h3>
+                <p class="text-center">Epicurus in armatum hostem impetum fecisse aut odit aut odit aut ad modum, quaeso, interpretaris? Consectum lorems dolor sit manet consectum lorem tuplal kkge sicine eos censes tantas. Lorem ipsum dolor sit amnet consectum libris set.</p>
+            </div>
+            <div class="grid_fullcell">
+                <img src="/wp-content/uploads/2021/06/70.png" alt="">
+            </div>
             <div class="grid_fullcell">
                 <img src="/wp-content/uploads/2021/06/abeille.png" alt="">
             </div>
             <div class="">
-                <h3 class="text-center mt-15">Nos valeurs</h3>
+                <h3 class="text-center mt-15">Nos <em>valeurs</em></h3>
                 <p class="text-center">Epicurus in armatum hostem impetum fecisse aut odit aut odit aut ad modum, quaeso, interpretaris? Consectum lorems dolor sit manet consectum lorem tuplal kkge sicine eos censes tantas. Lorem ipsum dolor sit amnet consectum libris set.</p>
             </div>
             <div class="">
-                <h3 class="text-center mt-15">Nos certifications</h3>
+                <h3 class="text-center mt-15">Nos <em>certifications</em></h3>
                 <p class="text-center">Epicurus in armatum hostem impetum fecisse aut odit aut odit aut ad modum, quaeso, interpretaris? Consectum lorems dolor sit manet consectum lorem tuplal kkge sicine eos censes tantas. Lorem ipsum dolor sit amnet consectum libris set.</p>
-                <p class="pt-2 bt-2 text-center"><a href="#" class="btn btn--green">Découvrir notre écostystème</a></p>
+<!--                <p class="pt-2 bt-2 text-center"><a href="#" class="btn btn--green">Découvrir notre écostystème</a></p>-->
             </div>
             <div class="grid_fullcell">
                 <img src="/wp-content/uploads/2021/06/70.png" alt="">
